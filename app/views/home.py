94a86470165e1e886be13e073f8243429616cb38
EEEden <edendoyle@inventati.org>
2016-02-05 17:38:45 +0000

    Added HTML form, added flask POST handler. Flask receives PML and spits it back out to a template

--- app/views/home.py
+++ app/views/home.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, Flask, render_template
+from flask import Blueprint, Flask, render_template, request, redirect
 from app import app
 
 home = Blueprint('home', __name__)
@@ -7,3 +7,9 @@ home = Blueprint('home', __name__)
 @home.route('/')
 def index():
     return render_template("home/index.html")
+
+@home.route('/pml_source_submit', methods=['POST'])
+def pml_source_submit():
+    source = request.form["pml_source"]
+
+    return render_template("home/pml_res.html", data = source)
