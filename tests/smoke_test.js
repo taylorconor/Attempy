60489e63046cb755b43fd6699d578ff803729137
Conor Taylor <taylorconor@me.com>
2016-03-13 18:39:57 +0000

    Finishing smoke tests

--- tests/smoke_test.js
+++ tests/smoke_test.js
@@ -100,6 +100,7 @@ var steps = [
       print_error("Are you sure the flask server is running?");
       return true;
     }
+    // check that the signin form is displayed (to prove that the page rendered properly)
     var test_elem = page.evaluate(function() {
       return document.querySelector(".form-signin");
     });
@@ -114,10 +115,12 @@ var steps = [
   function() {
     print_update("Testing feature: Authentication (Google OAuth)...");
     page.evaluate(function(click) {
+      // click the "sign in with Google" button
       click(document.querySelector("a[class~=btn-google]"));
     }, click);
   },
   function() {
+    // we should be redirected to Google's signin site
     if (!test_title("Sign in - Google Accounts", page)) { return true; }
     print_success("Authentication (Google OAuth) working!");
   },
@@ -129,10 +132,12 @@ var steps = [
   function() {
     print_update("Testing feature: Authentication (Facebook OAuth)...");
     page.evaluate(function(click) {
+      // click the "sign in with Facebook" button
       click(document.querySelector("a[class~=btn-facebook]"));
     }, click);
   },
   function() {
+    // we should be redirected to Facebook's signin site
     if (!test_title("Log into Facebook | Facebook", page)) { return true; }
     print_success("Authentication (Facebook OAuth) working!");
   },
@@ -144,10 +149,12 @@ var steps = [
   function() {
     print_update("Testing feature: Authentication (GitHub OAuth)...");
     page.evaluate(function(click) {
+      // click the "sign in with Github" button
       click(document.querySelector("a[class~=btn-twitter]"));
     }, click);
   },
   function() {
+    // we should be redirected to Github's signin site
     if (!test_title("Sign in to GitHub · GitHub", page)) { return true; }
     print_success("Authentication (GitHub OAuth) working!");
   },
@@ -157,11 +164,12 @@ var steps = [
   },
   // test 5: Local authentication
   function() {
-    page.open("http://lvh.me:5000/register");
     print_update("Testing feature: Authentication (local)...");
+    page.open("http://lvh.me:5000/register");
   },
   function() {
     if (!test_title("Register", page)) { return true; }
+    // add our test details to the "register" page
     page.evaluate(function() {
       document.querySelector("input[name=email]").setAttribute("value", "test@test.com");
       document.querySelector("input[name=name]").setAttribute("value", "Test User");
@@ -174,6 +182,7 @@ var steps = [
       print_error("Are you sure you purged the database before running the tests?");
       return true;
     }
+    // log in with the details we just registered with
     page.evaluate(function() {
       document.querySelector("input[name=email]").setAttribute("value", "test@test.com");
       document.querySelector("input[name=password]").setAttribute("value", "test");
@@ -181,6 +190,7 @@ var steps = [
     });
   },
   function() {
+    // we should now reach the editor page, which can only be reached after a successful sign in
     if (!test_title("Editor", page)) { return true; }
     var test_elem = page.evaluate(function() {
       return document.getElementById("nav-name");
@@ -194,12 +204,12 @@ var steps = [
   // end test 5
   // test 6: file upload
   function() {
+    print_update("Testing feature: File upload");
     var filename = fs.workingDirectory+"/testfile.pml";
     page.uploadFile("input[id=file]", filename);
     page.evaluate(function() {
       document.querySelector("form[id=upload]").submit();
     });
-    print_update("Testing feature: File upload");
   },
   function() {
     var text = page.evaluate(function() {
@@ -214,14 +224,16 @@ var steps = [
   // end test 6
   // test 7: file save
   function() {
+    print_update("Testing feature: File save");
     page.evaluate(function(click) {
+      // add some code to the editor
       ace.edit("editor").setValue("process P { broken_token }");
       click(document.querySelector("a[id=submit_save]"));
     }, click);
-    print_update("Testing feature: File save");
   },
   function() {
     page.evaluate(function(click) {
+      // save the file as "test_file.pml"
       document.querySelector("input[id=newFileName]").setAttribute("value", "test_file");
       click(document.querySelector("button[id=createNewName]"));
     }, click);
@@ -230,6 +242,7 @@ var steps = [
     var sidebar = page.evaluate(function(click) {
       return document.querySelector("ul[class~=tree]").outerHTML;
     });
+    // verify that "test_file.pml" is now shown in the sidebar
     if (sidebar.indexOf("test_file.pml") == -1) {
       print_error("File save didn't work!");
       return true;
@@ -239,12 +252,14 @@ var steps = [
   // end test 7
   // test 8: check syntax
   function() {
+    print_update("Testing feature: Check syntax");
     page.evaluate(function(click) {
+      // run "check syntax" on our broken code
       click(document.querySelector("a[id=check_syn]"));
     }, click);
-    print_update("Testing feature: Check syntax");
   },
   function() {
+    // the code is broken so the bell should go red
     var bell_style = page.evaluate(function() {
       return document.querySelector("i[id=syn_out_bell]").getAttribute("style");
     });
@@ -255,6 +270,95 @@ var steps = [
     print_success("Check syntax working!");
   },
   // end test 8
+  // test 9: error highlighting
+  function() {
+    print_update("Testing feature: Error highlighting");
+    var annotations = page.evaluate(function() {
+      return editor.getSession().getAnnotations();
+    });
+    // the code is broken, so there should be at least one error annotation
+    if (annotations.length != 1 || annotations[0].type !== "error") {
+      print_error("Error highlighting not working!");
+      return true;
+    }
+    print_success("Error highlighting working!");
+  },
+  //end test 9
+  function() {
+    page.open("http://lvh.me:5000");
+  },
+  // test 10: Keybinding emulation
+  function() {
+    print_update("Testing feature: Keybinding emulation");
+    page.evaluate(function(click) {
+      click(document.querySelector("a[id=editor_settings]"));
+    }, click);
+  },
+  function() {
+    var style = page.evaluate(function() {
+      return document.querySelector("div[id=editor_settings_pane]").getAttribute("style");
+    });
+    if (style != "display: block; ") {
+      print_error("Editor settings pane did not appear!");
+      return true;
+    }
+    page.evaluate(function() {
+      document.querySelector("select[id=keybinding_select]").selectedIndex = 1;
+    });
+  },
+  function() {
+    page.evaluate(function(click) {
+      click(document.querySelector("button[id=settings_done]"));
+    }, click);
+  },
+  function() {
+    var keybinding = page.evaluate(function() {
+      return ace.edit("editor").getKeyboardHandler();
+    });
+    if (keybinding != "") {
+      print_error("Keybinding not working!");
+      return true;
+    }
+    print_success("Keybinding emulation ok!");
+  },
+  // end test 10
+  function() {
+    page.open("http://lvh.me:5000");
+  },
+  // test 11: code completion
+  function() {
+    print_update("Testing feature: Code completion");
+    page.evaluate(function() {
+      // half-type the keyword "process"; the code completion should expand it
+      ace.edit("editor").setValue("proce");
+    });
+  },
+  function() {
+    var rect = page.evaluate(function() {
+      return document.querySelector("div[id=editor]").getBoundingClientRect();
+    });
+    // click inside the editor to give it focus
+    page.sendEvent('click', rect.left + rect.width / 2, rect.top + rect.height / 2);
+  },
+  function() {
+    // simulate Ctrl-Space, the shortcut to run the code completion
+    page.sendEvent('keypress', page.event.key.Space, null, null, 0x04000000);
+  },
+  function() {
+    // simulate Enter key to choose the first code completion suggestion
+    page.sendEvent('keypress', page.event.key.Return, null, null, 0x0);
+  },
+  function() {
+    var text = page.evaluate(function() {
+      return ace.edit("editor").getValue();
+    });
+    // code completion should have expanded "proce" to "process"
+    if (text != "process") {
+      print_error("Code completion didn't work!");
+      return true;
+    }
+    print_success("Code completion ok!");
+  },
 ];
 
 interval = setInterval(function() {
