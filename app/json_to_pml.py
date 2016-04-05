9e05f9f19760594a506035562dd201930706043f
EEEden <edendoyle@inventati.org>
2016-04-06 00:45:10 +0100

    Fixed infinite loop

--- app/json_to_pml.py
+++ app/json_to_pml.py
@@ -27,7 +27,6 @@ def joint_to_json(arr, name):
 		return res
 
 	def action(item):
-		print(str(item))
 		script = "\"" + "".join(item["scriptIn"]) + "\"" #Expects string below, convert to string
 		# agents = ",".join(item["AgentsIn"]) #Expects string too... potential conflict with representations
 				# requires = "".join(item["RequiresIn"])
@@ -45,6 +44,7 @@ def joint_to_json(arr, name):
 			"requires": requires,
 			"provides": provides
 		}
+
 
 	def other(item):
 		item = copy.deepcopy(item)
@@ -65,31 +65,31 @@ def joint_to_json(arr, name):
 		}
 	
 	while arr:
-		delete = []
-		for i, item in enumerate(arr):	
-			itemDict = {}
-			if item["elType"] == "action":
-				itemDict = action(item)					
-			else:
-				itemDict = other(item)
+            delete = []
+            for i, item in enumerate(arr):	
+                itemDict = {}
+                if item["elType"] == "action":
+                    itemDict = action(item)					
+                else:
+                    itemDict = other(item)
 
-			try: #nested
-				print processed
-				if item["parent"] in processed.keys():
-					processed[item["parent"]][len(processed[item["parent"]].keys())] = copy.deepcopy(itemDict)
-				else:
-					continue
-			except: #root level 
-				key = str(len(d["process"]["contains"].keys()))
-				d["process"]["contains"][key] = itemDict
-				if "embeds" in item.keys():
-					processed[item["id"]] = d["process"]["contains"][key]["contains"]
+                if "parent" in item.keys():
+                    if item["parent"] in processed.keys():
+                        key = str(len(processed[item["parent"]].keys()))
+                        processed[item["parent"]][key] = copy.deepcopy(itemDict)
+                        processed[item["id"]] = processed[item["parent"]][key]["contains"]
+                    else:
+                        continue
+                else: #root level 
+                    key = str(len(d["process"]["contains"].keys()))
+                    d["process"]["contains"][key] = itemDict
+                    if "embeds" in item.keys():
+                        processed[item["id"]] = d["process"]["contains"][key]["contains"]
 
-			delete.append(i)
-
-		for x in delete[::-1]:
-			print x
-			arr.pop(x)
+                delete.append(i)
+            for x in delete[::-1]:
+                arr.pop(x)
+        
 
 	return json.dumps(d)		
 			
@@ -197,7 +197,6 @@ def json_to_pml(json_str):
 
 def parse(arr, name):
     json_obj = joint_to_json(arr, name)
-    print json_obj
     return json_to_pml(json_obj)
 	
 if __name__ == '__main__':
@@ -206,9 +205,16 @@ if __name__ == '__main__':
 {"type":"html.Element","position":{"x":109.99996948242188,"y":130},"size":{"width":260,"height":50},"angle":0,"label":"Action","nameIn":"Action","scriptIn":[],"RequiresIn":[],"ProvidesIn":[],"AgentsIn":[],"id":"a8f85853-30f6-4d3f-93e3-d049d22a68e0","column":"","z":3,"parent":"dd5b87fd-dc2a-406d-bdd9-fea0376430b8","attrs":{}},
 {"type":"devs.Coupled","size":{"width":300,"height":50},"inPorts":[],"outPorts":[],"position":{"x":440,"y":90},"angle":0,"verticalChildCount":0,"id":"064c3e32-2953-4875-bc20-acba96246141","column":"","z":3,"attrs":{"text":{"text":"iteration"}}},
 {"type":"devs.Coupled","size":{"width":300,"height":50},"inPorts":[],"outPorts":[],"position":{"x":790,"y":90},"angle":0,"verticalChildCount":0,"id":"81abd23f-0d27-493f-84cf-04dbbd9d9a15","column":"","z":4,"attrs":{"text":{"text":"selection"}}}] 
-	
-	json_str = joint_to_json(test, "test")
-	print json_to_pml(json_str)
+
+        broken_test = [{"type":"devs.Coupled","size":{"width":730,"height":240},"inPorts":[],"outPorts":[],"position":{"x":54,"y":83},"angle":0,"class":"body branch","nameIn":"branch","elType":"branch","verticalChildCount":0,"id":"7ba82fc7-a220-47ba-98c8-7d12db944efe","column":"","z":900,"embeds":["90dab2df-c5ce-4bdc-9e0d-8d63dd7bb926","bba5ea5d-f148-43e9-871a-2a3f779af72d"],"attrs":{"text":{"text":"branch","class":"label branch"},"rect":{"class":"body branch","fill":"#ffffff"}}},
+{"type":"devs.Coupled","size":{"width":690,"height":110},"inPorts":[],"outPorts":[],"position":{"x":74,"y":193},"angle":0,"class":"body sequence","nameIn":"sequence","elType":"sequence","verticalChildCount":0,"id":"bba5ea5d-f148-43e9-871a-2a3f779af72d","column":"","z":901,"parent":"7ba82fc7-a220-47ba-98c8-7d12db944efe","embeds":["f5a6d8e4-28d7-4d06-aa9a-7b05acb137e4","a9cab37b-7c38-4e93-a122-cfd5d00c154a"],"attrs":{"text":{"text":"sequence","class":"label sequence"},"rect":{"class":"body sequence","fill":"#ffffff"}}},
+{"type":"devs.Coupled","size":{"width":300,"height":50},"inPorts":[],"outPorts":[],"position":{"x":74,"y":123},"angle":0,"label":"Action","elType":"action","nameIn":"","scriptIn":[],"RequiresIn":[],"ProvidesIn":[],"AgentsIn":[],"ToolsIn":[],"id":"90dab2df-c5ce-4bdc-9e0d-8d63dd7bb926","column":"","z":901,"parent":"7ba82fc7-a220-47ba-98c8-7d12db944efe","attrs":{"text":{"text":"action"},"rect":{"fill":"rgb(255,255,255)"}}},
+{"type":"devs.Coupled","size":{"width":300,"height":50},"inPorts":[],"outPorts":[],"position":{"x":444,"y":233},"angle":0,"label":"Action","elType":"action","nameIn":"","scriptIn":[],"RequiresIn":[],"ProvidesIn":[],"AgentsIn":[],"ToolsIn":[],"id":"a9cab37b-7c38-4e93-a122-cfd5d00c154a","column":"","z":902,"parent":"bba5ea5d-f148-43e9-871a-2a3f779af72d","attrs":{"text":{"text":"action"},"rect":{"fill":"rgb(255,255,255)"}}},
+{"type":"devs.Coupled","size":{"width":300,"height":50},"inPorts":[],"outPorts":[],"position":{"x":94,"y":233},"angle":0,"label":"Action","elType":"action","nameIn":"","scriptIn":[],"RequiresIn":[],"ProvidesIn":[],"AgentsIn":[],"ToolsIn":[],"id":"f5a6d8e4-28d7-4d06-aa9a-7b05acb137e4","column":"","z":902,"parent":"bba5ea5d-f148-43e9-871a-2a3f779af72d","attrs":{"text":{"text":"action"},"rect":{"fill":"rgb(255,255,255)"}}}]	
+	json_str = joint_to_json(broken_test, "test")
+	#print json_str
+    
+        print json_to_pml(json_str)
 
 
 	"""file_name = sys.argv[1]
